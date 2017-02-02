4d04b3c4acbe6bdca8bc70e2985366ab966e9791
chattes <gooner.sourav@gmail.com>
2017-02-02 11:57:01 +0530

    Stable Somehwat

--- controllers/xmovies8.js
+++ controllers/xmovies8.js
@@ -1,31 +1,19 @@
-const express = require('express')
-const fs = require('fs')
 const request = require('request')
 const cheerio = require('cheerio')
-const app = express()
 const NightMare = require('nightmare')
-const router = express.Router()
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors')
 const jsdom = require('jsdom')
-const xmovies = require('./controllers/xmovies8')
 
-app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-//Scrape URL and resolve to get Direct Video Link
-router.get('/resolve', function (req, res) {
-
+module.exports = {
+    index: function(req,res){
+        moviemodel = []
   //All the web scraping magic will happen here
-  console.log('Scrape')
-  // url = 'http://videohost.site/play/A11QStEaNdVZfvV/'
-  url = req.query.url
+  console.log('Scraping xmovies8...')
+  url = 'http://xmovies8.tv/language/hindi'
   if (url) {
     const nightmare = NightMare({show: false})
     nightmare
       .goto(url)
+      .wait('#movie-player')
       .evaluate(function () {
 
         // return document.body.innerHTML; //pass all of the html as text return
@@ -35,26 +23,17 @@ router.get('/resolve', function (req, res) {
           .body
           .getElementsByTagName("script")
         let scriptSources = []
-        let justMovies = 'justmoviesonline'
-        let autoplay = false
         for (let i = 0; i < tpScripts.length; i++) {
           if (tpScripts[i].src) {
-            if((tpScripts[i].src).indexOf(justMovies)!== -1){
-              autoplay = true
-            }
             scriptSources.push(tpScripts[i].src)
           }
-
-        }
-        if(autoplay){
-        jwplayer('Myp').play(true)
         }
         return {myHTML: document.body.innerHTML, scripts: scriptSources}
 
       })
       .end()
       .then(function (body) {
-        // console.log(body.myHTML)
+        //  console.log(body.myHTML)
         $ = cheerio.load(body.myHTML)
         let resolvedUrl = $('video').attr('src')
         res.json(resolvedUrl)
@@ -68,12 +47,7 @@ router.get('/resolve', function (req, res) {
     // TODO
     res.send('No URL Provided')
   }
-
-})
-
-router.get('/xmovies',xmovies.index)
-
-app.use(router)
-app.listen('3030')
-console.log('Magic happens on port 3030')
-exports = module.exports = app
+        
+    }
+    
+}
